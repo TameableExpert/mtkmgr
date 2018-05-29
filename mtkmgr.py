3a0ab0d586b7d93f67638148abb65c0662772fc6
Trevor White <trevorryanwhite@gmail.com>
2018-05-28 21:02:12 -0700

    Added checks for config folder, config file, and log files. Creates files if they are missing.

--- mtkmgr.py
+++ mtkmgr.py
@@ -2,12 +2,12 @@
 # MikroTik Manager - Making managing MikroTiks magical.
 #
 
-import sys, os, io
+import sys, os, io, json, datetime
 
 home_dir = os.path.expanduser('~') # Gets home dir crossplatform.
 config_folder = home_dir + "/.mtkmgr/"
-conf_file = config_folder + "mtkmgr.cfg"
-log_file = config_folder + "mtkmgr.log"
+conf_fileName = config_folder + "mtkmgr.cfg.json"
+log_fileName = config_folder + "mtkmgr.log"
 
 def Main():
     #
@@ -16,10 +16,18 @@ def Main():
     Setup()
 
 def Setup():
-    if os.path.exists(config_folder):
-        # TODO: Do something here.
-        print "I'll get to this later."
-    else:
-        os.mkdir(config_folder)
+    if not os.path.exists(config_folder):
+        os.mkdir(config_folder) # Create folder for saving config and log.
+        LogData("Directory created: " + config_folder)
+        LogData("File created: " + log_fileName)
+    if not os.path.exists(conf_fileName):
+        with open(conf_fileName, "w+") as conf_file:
+            conf_file.write(json.dumps({})) # Creates empty json file.
+            LogData("File created: " + conf_fileName)
+
+    
+def LogData(data):
+    with open(log_fileName, "a+") as log_file:
+        log_file.writelines(str(datetime.datetime.now()) + ": " + data + "\r")
 
 Main()
