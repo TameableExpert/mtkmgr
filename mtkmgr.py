1d4c83b8ef821494a278fe638f8d5de22bb25d61
Trevor White <trevorryanwhite@gmail.com>
2018-05-30 19:17:59 -0700

    Shuffled things around into a class and started working on the interface side.

--- mtkmgr.py
+++ mtkmgr.py
@@ -2,32 +2,48 @@
 # MikroTik Manager - Making managing MikroTiks magical.
 #
 
-import sys, os, io, json, datetime
+import sys, os, io, json, datetime, Tkinter
 
-home_dir = os.path.expanduser('~') # Gets home dir crossplatform.
-config_folder = home_dir + "/.mtkmgr/"
-conf_fileName = config_folder + "mtkmgr.cfg.json"
-log_fileName = config_folder + "mtkmgr.log"
+class mtkmgr():
+    def __init__(self):
+        #
+        # Setup
+        #
+        self.home_dir = os.path.expanduser('~') # Gets home dir crossplatform.
+        self.config_folder = self.home_dir + "/.mtkmgr/"
+        self.conf_fileName = self.config_folder + "mtkmgr.cfg.json"
+        self.log_fileName = self.config_folder + "mtkmgr.log"
 
-def Main():
-    #
-    # Run Setup - Create config_folder, conf_file, and log_file
-    #
-    Setup()
+        #
+        # Run Setup - Create config_folder, conf_file, and log_file
+        #
+        self.InitalSetup()
+        self.LoadConfig()
+        self.SetupInterface()
 
-def Setup():
-    if not os.path.exists(config_folder):
-        os.mkdir(config_folder) # Create folder for saving config and log.
-        LogData("Directory created: " + config_folder)
-        LogData("File created: " + log_fileName)
-    if not os.path.exists(conf_fileName):
-        with open(conf_fileName, "w+") as conf_file:
-            conf_file.write(json.dumps({})) # Creates empty json file.
-            LogData("File created: " + conf_fileName)
+    def InitalSetup(self):
+        #
+        # Create 
+        #
+        if not os.path.exists(self.config_folder):
+            #
+            # Create folder for saving config and log.
+            #
+            os.mkdir(self.config_folder)
+        self.LogData("Setup Complete.")
 
-    
-def LogData(data):
-    with open(log_fileName, "a+") as log_file:
-        log_file.writelines(str(datetime.datetime.now()) + ": " + data + "\r")
+    def LogData(self, data):
+        with open(self.log_fileName, "a+") as log_file:
+            formatted_data = str(datetime.datetime.now()) + ": " + data + "\r"
+            log_file.writelines(formatted_data)
+            print formatted_data # Print out to the terminal so I don't have to keep looking in the log file.
 
-Main()
+    def LoadConfig(self):
+        with open(self.conf_fileName, "w+") as conf_file:
+                conf_file.write(json.dumps({})) # Creates empty json file if config doesn't exist.
+
+    def SetupInterface(self):
+        self.app_window = Tkinter.Tk()
+        self.app_window.mainloop()
+
+mtkmgr()
