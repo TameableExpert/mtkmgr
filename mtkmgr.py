5be33e4d27099db45a865c7d80a16c9d46c28eb9
Trevor White <trevorryanwhite@gmail.com>
2018-06-22 18:21:48 -0700

    Listbox updates when new addresses are entered.

--- mtkmgr.py
+++ mtkmgr.py
@@ -44,28 +44,46 @@ class mtkmgr():
             with open(self.conf_fileName, "r+") as conf_file:
                     loaded_config = conf_file.read() # Creates empty json file if config doesn't exist.
                     if len(loaded_config) > 0:
-                        self.current_config = json.loads(loaded_config, encoding="utf-8")
+                        self.current_config = json.loads(loaded_config)
         else:
             with open(self.conf_fileName, "w+") as conf_file:
                     loaded_config = conf_file.read() # Creates empty json file if config doesn't exist.
                     if len(loaded_config) > 0:
-                        self.current_config = json.loads(loaded_config, encoding="utf-8")
+                        self.current_config = json.loads(loaded_config)
 
     def SaveConfig(self):
         with open(self.conf_fileName, "w+") as conf_file:
-            json_str = json.dumps(self.current_config, encoding="utf-8")
+            json_str = json.dumps(self.current_config)
             conf_file.write(json_str) # Creates empty json file if config doesn't exist.
 
     def AddHost(self):
         try:
-            keys =  enumerate(self.current_config)
-            if "hosts" not in keys:
-                self.current_config["hosts"] = {}
             ipaddress =  entry_ip_address.get()
-            self.current_config["hosts"][ipaddress] = {}
+            self.current_config['hosts'][ipaddress] = {}
             self.SaveConfig()
+            self.HostList_Load()
+            entry_ip_address.delete(0, 'end')
+            
         except Exception, err:
             print "Something went wrong!", err
+
+    def HostList_Clear(self):
+        listbox_hosts.delete(0, 'end')
+
+    def HostList_Load(self):
+        self.HostList_Clear()
+        if(self.current_config != {}):
+            # TODO:Check to see if config has hosts, if not, create a blank instance.
+                
+            # Build list using host records.
+            if len(self.current_config['hosts']) > 0:
+                for key, value in self.current_config['hosts'].items():
+                    listbox_hosts.insert(0, key) # Add host to listbox.
+                self.LogData("There are things here.")
+            else:
+                self.LogData("Error: No records found.")
+        else:
+            self.LogData("Error: Configuration file empty.")
 
     def ReturnSelectedHost(self):
         print listbox_hosts.selection_get()
@@ -82,20 +100,7 @@ class mtkmgr():
         label_hosts.pack()
         global listbox_hosts
         listbox_hosts = Tkinter.Listbox()
-
-        if(self.current_config != {}):
-            # TODO:Check to see if config has hosts, if not, create a blank instance.
-                
-            # Build list using host records.
-            if len(self.current_config["hosts"]) > 0:
-                for key, value in self.current_config["hosts"].items():
-                    listbox_hosts.insert(0, key) # Add host to listbox.
-                self.LogData("There are things here.")
-            else:
-                self.LogData("Error: No records found.")
-        else:
-            self.LogData("Error: Configuration file empty.")
-
+        self.HostList_Load()
         listbox_hosts.pack()
 
         # IP Address Entry
